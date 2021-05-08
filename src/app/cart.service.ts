9af12da7cf5aa45c613495e1550654c4ed7a37af
Ititriin <triin.tajur@vikk.ee>
2021-05-08 18:31:19 +0300

    remove unnecessary stuff

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -11,20 +11,8 @@ export class CartService {
     private http: HttpClient
   ) {}
 
-  addToCart(product) {
-    this.items.push(product);
-  }
-
   getItems() {
     return this.items;
   }
 
-  clearCart() {
-    this.items = [];
-    return this.items;
-  }
-
-  getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
-  }
 }
