1305df00aa3d9901993dc1478d357b044c7bc4cf
Ititriin <triin.tajur@vikk.ee>
2021-05-08 18:28:34 +0300

    remove unnecessary stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 
@@ -26,7 +25,6 @@ import { CartComponent } from './cart/cart.component';
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent
   ],
