f56145189260e40974955845c5d6816eb921afb6
Ititriin <triin.tajur@vikk.ee>
2021-05-08 19:19:16 +0300

    clear default stuff

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WikiComponent } from './wiki.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, HttpClientModule , RouterModule],
+  declarations: [ AppComponent, WikiComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/src/app/wiki.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  template: `
+    <h1>{{ data.titles.normalized }}</h1>
+    <p>{{data.description}}</p>
+    <img src="{{data.originalimage.source}}">  
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
+})
+export class WikiComponent {
+  data: any;
+  constructor(private http: HttpClient) {
+    http
+      .get('https://en.wikipedia.org/api/rest_v1/page/summary/Võru')
+      .subscribe(response => {
+        this.data = response;
+        //console.log(response);
+      });
+  }
+  @Input() name: string;
+}
